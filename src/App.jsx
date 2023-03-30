2411c1c4a0b63fa0edeab4a6bce9aa2ea974fedc
AbdullahWB <abdullah917828@gmail.com>
2023-03-30 17:36:50 +0600

    complete side card

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,19 @@ import Home from './Component/Home/Home';
 import Contact from './Component/Contact/Contact';
 import SideCart from './Component/SideCart/SideCart';
 import context from 'react-bootstrap/esm/AccordionContext';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [watchTime, setWatchTime] = useState("");
+  const [watchTime, setWatchTime] = useState(0);
   const handleWatchTime = (time) => {
-    const previousWatchTime = JSON.parse(localStorage.getItem('watch time'));
+    const previousWatchTime = JSON.parse(localStorage.getItem('watchTime'));
     if (previousWatchTime) {
       const sum = previousWatchTime + time;
-      localStorage.setItem('watch time', sum);
+      localStorage.setItem('watchTime', sum);
       setWatchTime(sum)
     } else {
-      localStorage.setItem('watch time', time);
+      localStorage.setItem('watchTime', time);
       setWatchTime(time)
     }
   };
@@ -36,6 +38,7 @@ function App() {
         </div>
       </div>
       <Contact></Contact>
+      <ToastContainer></ToastContainer>
     </div>
   )
 }
