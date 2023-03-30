2085fe1a1a37bd73a92e1dac3ca6cdf6249dc9d2
AbdullahWB <abdullah917828@gmail.com>
2023-03-30 16:23:54 +0600

    style side cart

--- src/Component/SideCart/SideCart.jsx
+++ src/Component/SideCart/SideCart.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 const SideCart = ({ watchTime }) => {
     return (
         <div>
-            <h1>this is side ber</h1>
+            <h1>My Cart</h1>
+
+            <div className="mt-5 text-center">
+                <p>total watch time</p>
+                <input type="text" value={0} disabled />
+            </div>
+            <h5 className="mt-5">Add Break time</h5>
+            <button
+                className="w-25 btn-circle m-1 btn btn-info"
+            >
+                15
+            </button>
+            <button className="w-25 btn-circle bg-warning btn btn-info">20</button>
+            <button className="w-25 btn-circle m-1 bg-danger btn btn-info">25</button>
+            <input type="text" value={0} disabled />
+            <button className="mt-5 btn btn-info w-100">
+                Complete
+            </button>
         </div>
     );
 };
